698bb082a7a4c0f641749268f9de08afaa0954b2
diefonro <dfonsecar@unal.edu.co>
2025-06-27 09:26:04 -0300

    UPDATE: Coordinator Navigation Pattern Implemented
    Coordinator Navigation Pattern implemented to handle easily and ordered the navigation between screens throughout the app.
    
    Added SelectCountriesScreen. Tested Coordinator with it.
    
    Moving into next feature.
    Feature completed.

--- El Buhonero/El Buhonero/Features/Custom LaunchScreen/ViewController/CustomLaunchScreenVC.swift
+++ El Buhonero/El Buhonero/Features/Custom LaunchScreen/ViewController/CustomLaunchScreenVC.swift
@@ -7,7 +7,9 @@
 
 import UIKit
 
-class CustomLaunchScreenVC: UIViewController, StoryboardInfo {
+class CustomLaunchScreenVC: UIViewController, StoryboardInfo, Coordinating {
+    
+    var coordinator: Coordinator?
     
     static var storyboard = "CustomLaunchScreen"
     static var identifier = "CustomLaunchScreenVC"
@@ -16,6 +18,8 @@ class CustomLaunchScreenVC: UIViewController, StoryboardInfo {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.coordinator?.hideNavigationBar(animated: true)
+        self.coordinator?.disableDragPopGesture()
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -24,6 +28,21 @@ class CustomLaunchScreenVC: UIViewController, StoryboardInfo {
             self.imageView.alpha = 1
             self.imageView.showScaleEffectAnimated()
         }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
+            self.pushToCountriesScreen()
+        }
     }
-
+    
+    func pushToCountriesScreen() {
+        UIView.animate(withDuration: 3.0) {
+            self.imageView.alpha = 1
+        } completion: { _ in
+            if let countriesScreen = UIStoryboard(name: SelectCountryScreenVC.storyboard, bundle: nil)
+                .instantiateViewController(withIdentifier: SelectCountryScreenVC.identifier) as? SelectCountryScreenVC {
+                countriesScreen.setCoordinator(coordinator: self.coordinator)
+                self.coordinator?.push(viewController: countriesScreen, animated: true)
+            }
+        }
+    }
 }
